b2ce79afabd11db60a670b6ad9c215abd984487d
skaur16 <me.sharandeepkaur16@gmail.com>
2024-01-22 21:28:17 +0530

    Main View Model created..

---
+++ app/src/main/java/com/example/auth1/MainViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.auth1
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel(){
+    //val firebase = FireBaseRepo()
+    //val profile = mutableStateOf(Profile())
+
+    var name = mutableStateOf("")
+    var age = mutableStateOf("")
+    var mail = mutableStateOf("")
+    var address = mutableStateOf("")
+
+    var finalname = mutableStateOf("")
+    var finalage = mutableStateOf("")
+    var finalmail = mutableStateOf("")
+    var finaladdress = mutableStateOf("")
+
+    //var listOfUsers = mutableStateOf(listOf(Profile()))
+
+    var submit = mutableStateOf(false)
+
+    /*fun toSendData() {
+        firebase.sendData(
+            finalname.value,
+            finalage.value,
+            finalmail.value,
+            finaladdress.value
+        )
+    }*/
+
+}
